c49b7e20e4901310342daa9c8db24a6603469865
GTB-Dazii <95157942+DazRepo@users.noreply.github.com>
2022-10-04 07:22:13 +0700

    Update quotes.py

--- EmikoRobot/modules/quotes.py
+++ EmikoRobot/modules/quotes.py
@@ -62,7 +62,7 @@ async def quotly(event):
         file = await create_quotly(reply_, bg=match, reply=replied_to, sender=user)
     except Exception as er:
         return await msg.edit(f"ERROR: {er}")
-    message = await reply.reply("Quotly by Dazzi", file=file)
+    message = await reply.reply("Quotly by Stream Bot", file=file)
     remove(file)
     await msg.delete()
     return message
