3474a086729c730091eb8801a0e500b4359f7b00
GTB-Dazii <95157942+DazRepo@users.noreply.github.com>
2022-10-09 18:38:14 +0700

    Update global_bans.py

--- EmikoRobot/modules/global_bans.py
+++ EmikoRobot/modules/global_bans.py
@@ -413,6 +413,44 @@ def gbanlist(update: Update, context: CallbackContext):
         )
 
 
+def check_and_ban(update, user_id, should_message=True):
+
+    if user_id in TIGERS or user_id in WOLVES:
+        sw_ban = None
+    else:
+        try:
+            sw_ban = sw.get_ban(int(user_id))
+        except:
+            sw_ban = None
+
+    if sw_ban:
+        update.effective_chat.ban_member(user_id)
+        if should_message:
+            update.effective_message.reply_text(
+                f"<b>Alert</b>: this user is globally banned.\n"
+                f"<code>*bans them from here*</code>.\n"
+                f"<b>Appeal chat</b>: {SPAMWATCH_SUPPORT_CHAT}\n"
+                f"<b>User ID</b>: <code>{sw_ban.id}</code>\n"
+                f"<b>Ban Reason</b>: <code>{html.escape(sw_ban.reason)}</code>",
+                parse_mode=ParseMode.HTML,
+            )
+        return
+
+    if sql.is_user_gbanned(user_id):
+        update.effective_chat.ban_member(user_id)
+        if should_message:
+            text = (
+                f"<b>Alert</b>: this user is globally banned.\n"
+                f"<code>*bans them from here*</code>.\n"
+                f"<b>Appeal chat</b>: @{SUPPORT_CHAT}\n"
+                f"<b>User ID</b>: <code>{user_id}</code>"
+            )
+            user = sql.get_gbanned_user(user_id)
+            if user.reason:
+                text += f"\n<b>Ban Reason:</b> <code>{html.escape(user.reason)}</code>"
+            update.effective_message.reply_text(text, parse_mode=ParseMode.HTML)
+
+
 def enforce_gban(update: Update, context: CallbackContext):
     # Not using @restrict handler to avoid spamming - just ignore if cant gban.
     bot = context.bot
