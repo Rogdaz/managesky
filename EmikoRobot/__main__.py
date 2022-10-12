0ee81cfb1bf42b0a6456aab556132e9c10492d98
GTB-Dazii <95157942+DazRepo@users.noreply.github.com>
2022-10-12 08:58:33 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -420,7 +420,7 @@ def emiko_about_callback(update, context):
         )
     elif query.data == "emiko_support":
         query.message.edit_text(
-            text="๏ *{dispatcher.bot.first_name}* support chats"
+            text="*๏›› STREAM BOT support chats*"
             f"\nJoin My Support Group/Channel for see or report a problem on {dispatcher.bot.first_name}.",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
