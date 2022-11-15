4362285266dca498741322f88a37c0be43692eb9
GTB-Dazii <95157942+DazRepo@users.noreply.github.com>
2022-11-15 19:42:01 +0700

    Update country.py

--- EmikoRobot/modules/country.py
+++ EmikoRobot/modules/country.py
@@ -111,3 +111,10 @@ Gathered By Stream Robot ✨.</b>
     )
 
     await event.delete()
+
+__help__ = """
+I will give information about a country
+ ❂ /country <country name>*:* Gathering info about given country
+"""
+
+__mod_name__ = "Country"
